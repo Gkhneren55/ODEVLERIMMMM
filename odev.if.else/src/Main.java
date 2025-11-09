0aaac90753866da7bdbed9c2dc0029f455474476
Gökhan Eren <gokhanerenacar2011@gmail.com>
2025-11-09 20:38:16 +0300

    Add files via upload

---
+++ odev.if.else/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int not1=53;
+    int not2=98;
+    double sonuc1=not1*0.4;
+            double sonuc2=not2*0.6;
+                    if (sonuc1+sonuc2>75){
+                        System.out.println("barajı geçtin");
+                    }else if (sonuc1+sonuc2<75){
+                            System.out.println("kaldın");
+                        }}
