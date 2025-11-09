92f65407e3d639320957bcf44ce7ada6db5fe744
Gökhan Eren <gokhanerenacar2011@gmail.com>
2025-11-09 20:34:45 +0300

    Add files via upload

---
+++ odev.strin/src/Main.java
@@ -0,0 +1,8 @@
+void main(){
+    String metin1="merhaba";
+    char oyle= metin1.charAt(2);
+    System.out.println(oyle);
+    String metin2="Samsunspor";
+    String buyukharf= metin2.toUpperCase();
+    System.out.println(buyukharf);
+}
